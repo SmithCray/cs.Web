650f52e90738443cdc678b987ab0858f4dcc57f2
Cray Smith <cmsmith004@gmail.com>
2022-11-28 12:20:47 -0700

    added dark mode btn and transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,3 @@
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs((slideIndex += n));
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("slides");
-//   if (n > x.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = x.length;
-//   }
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   x[slideIndex - 1].style.display = "block";
-// }
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-// }
 let slideIndex = 0;
 showSlides();
 
@@ -65,5 +12,10 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 8000); // Change image every 2 seconds
+  setTimeout(showSlides, 8000);
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
